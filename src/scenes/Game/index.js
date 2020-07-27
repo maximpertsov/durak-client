989443d243afcb150b2a91d609e51a6385609da1
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-07-26 20:13:09 -0400

    display a card

--- src/scenes/Game/index.js
+++ src/scenes/Game/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import Card from './components/Card';
+
 const Game = () => (
-  <div className="Game" />
+  <div className="Game">
+    <Card />
+  </div>
 );
 
 export default Game;
