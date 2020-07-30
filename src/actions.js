21fa26882b08e0398ce29f7394f6c342f34dc5d8
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-07-30 00:22:38 -0400

    new actions/reducers

--- src/actions.js
+++ src/actions.js
@@ -1,7 +1,13 @@
 import { createActions } from 'redux-actions';
 
 export default createActions({
+  MESSAGES: {
+    APPEND: undefined,
+  },
   GAME: {
+    ATTACKER: {
+      SET: undefined,
+    },
     HAND: {
       APPEND: undefined,
       REMOVE: undefined,
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,18 +2,37 @@ import { combineReducers } from 'redux';
 import { handleActions } from 'redux-actions';
 
 import actions from 'actions';
+import update from 'immutability-helper';
 
-import hands from './hands';
+import hands, { players } from './hands';
 import table from './table';
 
+// constants
+
+// helpers
+const set = (state, action) => action.payload;
+const append = (state, action) => update(state, { $push: [action.payload] });
+
 const rootReducer = combineReducers({
+  attacker: handleActions(
+    {
+      [actions.game.attacker.set]: set,
+    },
+    null,
+  ),
   hands,
+  messages: handleActions(
+    {
+      [actions.messages.append]: append,
+    },
+    [],
+  ),
   table,
   username: handleActions(
     {
-      [actions.game.username.set]: (state, payload) => payload,
+      [actions.game.username.set]: set,
     },
-    'anna',
+    players[0],
   ),
 });
 
