6d5ba9101ed936f0b11a29cc6f9816025929328a
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-08-05 01:24:37 -0400

    show attacker

--- src/scenes/Game/components/Hand.js
+++ src/scenes/Game/components/Hand.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { createSelector } from 'reselect';
+import first from 'lodash/first';
 import isEqual from 'lodash/isEqual';
 
 import Cards from './Cards';
@@ -13,18 +14,25 @@ const mapStateToProps = createSelector(
 
   (state, user) => ({
     cards: state.hands[user],
+    isInitialAttacker: first(state.players) === user,
     user,
   }),
 );
 
 const Hand = () => {
-  const { cards, user } = useSelector(mapStateToProps, isEqual);
+  const { cards, isInitialAttacker, user } = useSelector(
+    mapStateToProps,
+    isEqual,
+  );
 
   return (
     <div className="Hand">
       <YieldButton />
       <CollectButton />
-      <h2>{user}</h2>
+      <h2>
+        {user}
+        {isInitialAttacker ? '*' : ''}
+      </h2>
       <Cards cards={cards} />
     </div>
   );
--- src/scenes/Game/components/Player.js
+++ src/scenes/Game/components/Player.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import first from 'lodash/first';
 import isEqual from 'lodash/isEqual';
 
 import Cards from './Cards';
 
 const Player = ({ player }) => {
   const cards = useSelector(state => state.hands[player], isEqual);
+  const players = useSelector(state => state.players, isEqual);
 
   // TODO: this should not be visible in a real game
   if (!player) return <div />;
 
+  const isInitialAttacker = first(players) === player;
+
   return (
     <div className="Player">
-      <h2>{player}</h2>
+      <h2>
+        {player}
+        {isInitialAttacker ? '*' : ''}
+      </h2>
       <Cards cards={cards} />
+      <div />
     </div>
   );
 };
