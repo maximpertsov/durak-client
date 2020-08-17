3ee0947eece743e7133daca597ff8292e9189dfd
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-08-17 19:39:43 -0400

    just show if other players are defending

--- src/scenes/Game/components/Player.js
+++ src/scenes/Game/components/Player.js
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import { createSelector } from 'reselect';
 
-import { getAttackers, getDefender } from 'reducers';
+import { getDefender } from 'reducers';
 import fp from 'utils/lodashFp';
 
 import Cards from './Cards';
@@ -18,35 +18,27 @@ const getDisplayCards = fp.flow(
 
 const mapStateToProps = createSelector(
   state => state,
-  state => getAttackers(state),
   (_, props) => props.player,
   (state, props) => state.hands[props.player],
 
-  (state, attackers, player, cards) => ({
+  (state, player, cards) => ({
     isDefender: getDefender(state) === player,
-    isInitialAttacker: fp.first(attackers) === player,
     cardCount: getCardCount(cards),
     displayCards: getDisplayCards(cards),
   }),
 );
 
 const Player = ({ player }) => {
-  const {
-    cardCount,
-    displayCards,
-    isDefender,
-    isInitialAttacker,
-  } = useSelector(state => mapStateToProps(state, { player }), fp.isEqual);
+  const { cardCount, displayCards, isDefender } = useSelector(
+    state => mapStateToProps(state, { player }),
+    fp.isEqual,
+  );
 
   if (!player) return <div />;
 
   return (
     <div className="Player">
-      <h2>
-        {`${player}${isInitialAttacker ? ' *' : ''}${
-          isDefender ? String.fromCodePoint(0x1f6e1) : ''
-        }`}
-      </h2>
+      <h2>{`${player}${isDefender ? String.fromCodePoint(0x1f6e1) : ''}`}</h2>
       <Cards flipped cards={displayCards} />
       <div>{`${cardCount} cards`}</div>
       <div />
