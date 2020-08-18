e0b6ace2f4a4a720dae16c5ae9648a13c3633b52
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-08-18 00:35:35 -0400

    Squashed commit of the following:
    commit e9e9c257121cc826c6a3fbd9f60335f2544822d4
    Author: Maxim Pertsov <maxim.pertsov@gmail.com>
    Date:   Tue Aug 18 00:35:05 2020 -0400
    
        remove tenguykitchen asset
    
    commit 03e9b0b812632e793210f37190a13c6b51817ee2
    Author: Maxim Pertsov <maxim.pertsov@gmail.com>
    Date:   Tue Aug 18 00:31:24 2020 -0400
    
        ten guy

--- src/scenes/Game/index.js
+++ src/scenes/Game/index.js
@@ -4,11 +4,17 @@ import { createSelector } from 'reselect';
 import styled from '@emotion/styled';
 import { Dimmer, Loader } from 'semantic-ui-react';
 
+import compact from 'lodash/compact';
+import first from 'lodash/first';
+import flatMap from 'lodash/flatMap';
+import isEmpty from 'lodash/isEmpty';
 import isEqual from 'lodash/isEqual';
+import size from 'lodash/size';
 import zipObject from 'lodash/zipObject';
 
 import { getPlayersFromUser } from 'reducers';
 
+import tenguyscrambled from './assets/tenguyscrambled.png';
 import DrawPile from './components/DrawPile';
 import GameInitializer from './components/GameInitializer';
 import Hand from './components/Hand';
@@ -18,12 +24,24 @@ import Player from './components/Player';
 import Table from './components/Table';
 import WebSocketEventListener from './components/WebSocketEventListener';
 
+const getDurak = ({ drawPile, hands }) => {
+  if (!isEmpty(drawPile)) return null;
+
+  const playersWithCards = flatMap(hands, (hand, player) =>
+    (isEmpty(compact(hand)) ? [] : [player]),
+  );
+  if (size(playersWithCards) !== 1) return null;
+
+  return first(playersWithCards);
+};
+
 const mapStateToProps = createSelector(
   state => state,
   state => getPlayersFromUser(state),
 
   (state, playersFromUser) => ({
     hands: state.hands,
+    isDurak: getDurak(state) === state.user,
     isLoading: state.remoteDataState !== 'REPLAYED_EVENTS',
     ...zipObject(['user', 'player2', 'player3', 'player4'], playersFromUser),
   }),
@@ -42,19 +60,24 @@ const TableWrapper = styled.div({
 });
 
 const Game = () => {
-  const { isLoading, player2, player3, player4 } = useSelector(
+  const { isLoading, isDurak, player2, player3, player4 } = useSelector(
     mapStateToProps,
     isEqual,
+  );
+
+  const renderTable = () => (
+    <TableWrapper>
+      <Table />
+      <PassCards />
+    </TableWrapper>
   );
 
   const renderGame = () => (
     <Wrapper>
       <div>
-        <Messages />
-        <TableWrapper>
-          <Table />
-          <PassCards />
-        </TableWrapper>
+        {!isDurak && <Messages />}
+        {!isDurak && renderTable()}
+        {isDurak && <img src={tenguyscrambled} alt="durak" />}
         <Hand />
       </div>
       <div>
