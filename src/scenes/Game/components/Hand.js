7fc3f0136e43000358e5e66540ad49c0f4496a58
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-08-17 20:49:16 -0400

    done message

--- src/scenes/Game/components/Hand.js
+++ src/scenes/Game/components/Hand.js
@@ -17,24 +17,31 @@ const mapStateToProps = createSelector(
     cards: state.hands[state.user],
     isDefender: getDefender(state) === state.user,
     isInitialAttacker: fp.first(attackers) === state.user,
+    isInGame: state.players.includes(state.user),
     user: state.user,
   }),
 );
 
 const Hand = () => {
-  const { cards, isDefender, isInitialAttacker, user } = useSelector(
+  const { cards, isDefender, isInitialAttacker, isInGame, user } = useSelector(
     mapStateToProps,
     fp.isEqual,
   );
 
   const dagger = String.fromCodePoint(0x1f5e1);
   const shield = String.fromCodePoint(0x1f6e1);
+  const popcorn = String.fromCodePoint(0x1f37f);
 
   return (
     <div className="Hand">
       <YieldButton />
       <CollectButton />
-      {isInitialAttacker && <h2>{`${`${dagger} You are attacking ${dagger}`}`}</h2>}
+      {!isInGame && (
+        <h2>{`${`${popcorn} Relax, you're not the durak! ${popcorn}`}`}</h2>
+      )}
+      {isInitialAttacker && (
+        <h2>{`${`${dagger} You are attacking ${dagger}`}`}</h2>
+      )}
       {isDefender && <h2>{`${`${shield} You are defending ${shield}`}`}</h2>}
       <h2>{user}</h2>
       <Cards cards={cards} />
