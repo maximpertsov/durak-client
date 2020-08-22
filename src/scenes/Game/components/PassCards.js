7e4dac42d677f388d85e35fa8f72a622fe8bcba7
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-08-21 23:26:19 -0400

    fix alignment of pass drop zone

--- src/scenes/Game/components/PassCards.js
+++ src/scenes/Game/components/PassCards.js
@@ -41,6 +41,7 @@ const mapStateToProps = createSelector(
 const Wrapper = styled.div(props => ({
   backgroundColor: 'blue',
   height: '250px',
+  margin: '10px',
   opacity: props.isOver ? '90%' : '100%',
   padding: '10px',
   width: '20%',
