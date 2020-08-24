ac5ca5e361e869fd5cc2dacf79f01b5a7a8b46fb
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-08-23 23:54:05 -0400

    reduce table height slightly

--- src/scenes/Game/components/PassCards.js
+++ src/scenes/Game/components/PassCards.js
@@ -40,7 +40,7 @@ const mapStateToProps = createSelector(
 
 const Wrapper = styled.div(props => ({
   backgroundColor: 'blue',
-  height: '50vh',
+  height: '40vh',
   margin: '10px',
   opacity: props.isOver ? '90%' : '100%',
   padding: '10px',
