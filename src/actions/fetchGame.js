cfaf61672e9cc9d5474dc0722538a7ed5b29a6d5
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-08-25 23:25:47 -0400

    set pass count zero at new game

--- src/actions/fetchGame.js
+++ src/actions/fetchGame.js
@@ -19,6 +19,9 @@ const fetchGame = ({ game }) => dispatch => {
       data: { drawPile, players },
     } = response;
 
+    // clear draws
+    dispatch(actions.game.passCount.set(0));
+
     // clear table
     dispatch(actions.game.table.clear());
 
