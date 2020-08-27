94ce0976f87d6e6d80d7383726a1a25c3d703301
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-08-27 01:15:23 -0400

    get token from server

--- src/actions/login.js
+++ src/actions/login.js
@@ -1,7 +1,7 @@
 import actions from 'actions';
 import updateLoginForm from 'actions/updateLoginForm';
-import Cookies from 'js-cookie';
-import fp from 'utils/lodashFp';
+import jwtDecode from 'jwt-decode';
+import client from 'utils/client';
 
 // TODO: obviously for testing only
 const userPasswords = {
@@ -15,17 +15,17 @@ const userPasswords = {
 
 const login = ({ username, password }) => async dispatch => {
   try {
-    if (fp.get(username, userPasswords) !== password) {
-      throw new Error();
-    }
-    dispatch(actions.game.user.set(username));
-    // create cookie that expires in 1 hour
-    // https://github.com/js-cookie/js-cookie/issues/74
-    // https://github.com/js-cookie/js-cookie/wiki/Frequently-Asked-Questions#expire-cookies-in-less-than-a-day
-    Cookies.set('sid', username, { expires: 1 / 24 });
+    const {
+      data: { access, refresh },
+    } = await client.post('token', { username, password });
+    const { user } = jwtDecode(access);
+
+    localStorage.setItem('access', access);
+    localStorage.setItem('refresh', refresh);
+
+    dispatch(actions.game.user.set(user));
   } catch (error) {
     dispatch(updateLoginForm({ error: 'Login failed' }));
-    Cookies.remove('sid');
     dispatch(actions.game.user.set(null));
   } finally {
     dispatch(updateLoginForm({ username: '', password: '' }));
