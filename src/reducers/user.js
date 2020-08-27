3dbbe5ef0e8b8cde3fac7691011df22a9ff2e35b
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-08-27 02:11:34 -0400

    initial user is always null

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,17 +1,14 @@
 import { handleActions } from 'redux-actions';
 
 import actions from 'actions';
-import Cookies from 'js-cookie';
 
 const set = (state, action) => action.payload;
-
-const getInitialValue = () => Cookies.get('sid') || null;
 
 const user = handleActions(
   {
     [actions.game.user.set]: set,
   },
-  getInitialValue(),
+  null,
 );
 
 export default user;
