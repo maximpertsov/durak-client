827a13aae6c767c10a2577f58b0f5ebb63aba431
maximpertsov <maxim.pertsov@gmail.com>
2020-08-28 14:00:34 -0400

    Reorganize hud (#26)
    * remove redundant user from hand
    
    * More main message to top
    
    * Pull out restart button
    
    * move button to game component

--- src/scenes/Game/index.js
+++ src/scenes/Game/index.js
@@ -2,30 +2,69 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { createSelector } from 'reselect';
 import styled from '@emotion/styled';
-import { Dimmer, Loader } from 'semantic-ui-react';
+import { Dimmer, Loader, Segment } from 'semantic-ui-react';
 
+import compact from 'lodash/compact';
+import first from 'lodash/first';
+import flatMap from 'lodash/flatMap';
+import isEmpty from 'lodash/isEmpty';
 import isEqual from 'lodash/isEqual';
+import size from 'lodash/size';
 import zipObject from 'lodash/zipObject';
 
-import { getGame, getPlayersFromUser } from 'reducers';
+import {
+  getAttackers,
+  getDefender,
+  getGame,
+  getPlayersFromUser,
+} from 'reducers';
 
+import CollectButton from './components/CollectButton';
 import DrawPile from './components/DrawPile';
 import GameInitializer from './components/GameInitializer';
 import Hand from './components/Hand';
 import Messages from './components/Messages';
 import PassCards from './components/PassCards';
 import Player from './components/Player';
+import RestartButton from './components/RestartButton';
 import Table from './components/Table';
 import WebSocketEventListener from './components/WebSocketEventListener';
+import YieldButton from './components/YieldButton';
+
+const dagger = String.fromCodePoint(0x1f5e1);
+const shield = String.fromCodePoint(0x1f6e1);
+const popcorn = String.fromCodePoint(0x1f37f);
+const rofl = String.fromCodePoint(0x1f923);
+
+const getDurak = ({ drawPile, hands }) => {
+  if (!isEmpty(drawPile)) return null;
+
+  const playersWithCards = flatMap(hands, (hand, player) => {
+    if (isEmpty(compact(hand))) return [];
+
+    return [player];
+  });
+
+  if (size(playersWithCards) !== 1) return null;
+
+  return first(playersWithCards);
+};
 
 const mapStateToProps = createSelector(
   state => state,
   state => getPlayersFromUser(state),
+  state => getDefender(state),
+  state => getDurak(state),
 
-  (state, playersFromUser) => ({
+  (state, playersFromUser, defender, durak) => ({
+    defender,
     game: getGame(),
     hands: state.hands,
     isLoading: state.remoteDataState !== 'REPLAYED_EVENTS',
+    isAttacker: getAttackers(state).includes(state.user),
+    isDefender: defender === state.user,
+    isOutOfGame: durak !== null || !state.players.includes(state.user),
+    isDurak: durak === state.user,
     user: state.user,
     ...zipObject(['player1', 'player2', 'player3', 'player4'], playersFromUser),
   }),
@@ -43,11 +82,24 @@ const TableWrapper = styled.div({
   display: 'flexbox',
 });
 
+const ButtonWrapper = styled.div({
+  marginBottom: '15px',
+});
+
 const Game = () => {
-  const { isLoading, game, user, player2, player3, player4 } = useSelector(
-    mapStateToProps,
-    isEqual,
-  );
+  const {
+    defender,
+    isAttacker,
+    isDefender,
+    isDurak,
+    isOutOfGame,
+    isLoading,
+    game,
+    player2,
+    player3,
+    player4,
+    user,
+  } = useSelector(mapStateToProps, isEqual);
 
   const renderTable = () => (
     <TableWrapper>
@@ -56,10 +108,44 @@ const Game = () => {
     </TableWrapper>
   );
 
+  const getMessage = () => {
+    if (isDurak) return `${rofl} You're the durak! ${rofl}`;
+    if (isOutOfGame) {
+      return `${popcorn} Relax, you're not the durak! ${popcorn}`;
+    }
+    if (isAttacker) return `${dagger} You are attacking ${defender} ${dagger}`;
+    if (isDefender) return `${shield} You are defending ${shield}`;
+
+    return null;
+  };
+
+  const renderTopMessage = () =>
+    getMessage() && (
+      <Segment tertiary>
+        <h2>{getMessage()}</h2>
+      </Segment>
+    );
+
+  const renderButton = () => {
+    if (isDurak) return <RestartButton />;
+    if (isOutOfGame) return null;
+    if (isDefender) return <CollectButton />;
+
+    return <YieldButton />;
+  };
+
+  const renderButtons = () => (
+    <ButtonWrapper>
+      <div>{renderButton()}</div>
+    </ButtonWrapper>
+  );
+
   const renderGame = () => (
     <Wrapper>
       <div>
+        {renderTopMessage()}
         {renderTable()}
+        {renderButtons()}
         <Hand />
         <Messages />
       </div>
