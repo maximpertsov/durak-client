1d7c16bd67c5f63e0e1cd8b8876871e47d805437
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-08-28 02:22:52 -0400

    login/nav are global

--- src/App.js
+++ src/App.js
@@ -6,9 +6,13 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Game from 'scenes/Game';
 import Home from 'scenes/Home';
+import LoginForm from 'scenes/Home/components/LoginForm';
+import Navigation from 'scenes/Home/components/Navigation';
 
 const App = () => (
   <div className="App">
+    <Navigation />
+    <LoginForm />
     <Router>
       <Route exact path="/" component={Home} />
       <Route exact path="/:game" component={Game} />
--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 
 import GameList from './components/GameList';
-import LoginForm from './components/LoginForm';
-import Navigation from './components/Navigation';
 
 const Home = () => (
   <div className="Home">
-    <Navigation />
-    <LoginForm />
     <GameList />
   </div>
 );
