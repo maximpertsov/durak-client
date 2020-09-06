2e092e8258d63b972a7ba73b8b87ebb06f50c4cd
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-09-06 11:46:22 -0400

    give up then collect flow

--- src/scenes/Game/components/CollectButton.js
+++ src/scenes/Game/components/CollectButton.js
@@ -6,15 +6,22 @@ import { Button } from 'semantic-ui-react';
 import isEmpty from 'lodash/isEmpty';
 import isEqual from 'lodash/isEqual';
 
-import { getDefender, getHands, getTable, getUnbeatenCards } from 'reducers';
+import {
+  getCollector,
+  getDefender,
+  getTable,
+  getUnbeatenCards,
+} from 'reducers';
 import { useWebSocketContext } from 'utils/websockets';
+
+const COLLECT_DELAY = 3000;
 
 const mapStateToProps = createSelector(
   state => state,
   state => state.user,
 
   (state, user) => ({
-    hands: getHands(state),
+    isCollecting: user === getCollector(state),
     isDefender: user === getDefender(state),
     table: getTable(state),
     user,
@@ -22,24 +29,34 @@ const mapStateToProps = createSelector(
   }),
 );
 
+// TODO: rename to CollectHandler to capture extra collect logic
 const CollectButton = () => {
   const io = useWebSocketContext();
 
-  const { isDefender, table, unbeatenCards } = useSelector(
+  const { isCollecting, isDefender, table, unbeatenCards } = useSelector(
     mapStateToProps,
     isEqual,
   );
 
-  const collectCards = () => {
-    io.send('collected', {});
+  const giveUp = () => {
+    io.send('gave_up', {});
   };
 
+  React.useEffect(() => {
+    if (isCollecting) {
+      setTimeout(() => {
+        io.send('collect_cards', {});
+      }, COLLECT_DELAY);
+    }
+  }, [isCollecting, io]);
+
+  if (isCollecting) return null;
   if (!isDefender) return null;
   if (isEmpty(table)) return null;
   if (isEmpty(unbeatenCards)) return null;
 
   return (
-    <Button circular size="big" onClick={collectCards}>
+    <Button circular size="big" onClick={giveUp}>
       collect cards
     </Button>
   );
