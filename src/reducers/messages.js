cebbbacc13901f833202cf74d2bd5f9f42ba642c
maximpertsov <maxim.pertsov@gmail.com>
2020-09-07 11:53:50 -0400

    Various fixes (#36)
    * lock cards once there is a durak
    
    * reduce collection period to 4 seconds
    
    * clear game events when returning to lobby

--- src/reducers/messages.js
+++ src/reducers/messages.js
@@ -4,10 +4,12 @@ import actions from 'actions';
 import update from 'immutability-helper';
 
 const append = (state, action) => update(state, { $push: [action.payload] });
+const clear = () => [];
 
 const messages = handleActions(
   {
     [actions.messages.append]: append,
+    [actions.messages.clear]: clear,
   },
   [],
 );
