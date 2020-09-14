82fec88c1c81657aab2fa476235f4dce7605862f
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-09-13 21:21:30 -0400

    spike organization buttons

--- src/scenes/Game/components/Hand.js
+++ src/scenes/Game/components/Hand.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { createSelector } from 'reselect';
+import { Button } from 'semantic-ui-react';
 
 import get from 'lodash/get';
 import isEqual from 'lodash/isEqual';
 
 import { getHands } from 'reducers';
+import { useWebSocketContext } from 'utils/websockets';
 
 import Cards from './Cards';
 
@@ -18,11 +20,21 @@ const mapStateToProps = createSelector(
 );
 
 const Hand = () => {
+  const io = useWebSocketContext();
   const { cards } = useSelector(mapStateToProps, isEqual);
 
   return (
     <div className="Hand">
       <Cards cards={cards} />
+      <div>Organize cards</div>
+      <Button
+        content="By rank"
+        onClick={() => {
+          io.send('organized', { strategy: 'group_by_rank' });
+        }}
+      />
+      <Button content="By suit" />
+      <Button content="By rank and trump" />
     </div>
   );
 };
