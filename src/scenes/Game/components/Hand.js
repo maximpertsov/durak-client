4a729a9f9ad1d5bf509bd79b7457676aba796290
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-09-13 21:45:21 -0400

    defined organizing strategies

--- src/scenes/Game/components/Hand.js
+++ src/scenes/Game/components/Hand.js
@@ -19,6 +19,17 @@ const mapStateToProps = createSelector(
   }),
 );
 
+const groupByRank = io => () => {
+  io.send('organized', { strategy: 'group_by_rank' });
+};
+
+const groupBySuit = io => () => {
+  io.send('organized', { strategy: 'group_by_suit' });
+};
+const groupByRankAndTrump = io => () => {
+  io.send('organized', { strategy: 'group_by_rank_and_trump' });
+};
+
 const Hand = () => {
   const io = useWebSocketContext();
   const { cards } = useSelector(mapStateToProps, isEqual);
@@ -27,14 +38,9 @@ const Hand = () => {
     <div className="Hand">
       <Cards cards={cards} />
       <div>Organize cards</div>
-      <Button
-        content="By rank"
-        onClick={() => {
-          io.send('organized', { strategy: 'group_by_rank' });
-        }}
-      />
-      <Button content="By suit" />
-      <Button content="By rank and trump" />
+      <Button content="By rank" onClick={groupByRank(io)} />
+      <Button content="By suit" onClick={groupBySuit(io)} />
+      <Button content="By rank and trump" onClick={groupByRankAndTrump(io)} />
     </div>
   );
 };
