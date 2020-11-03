1fa9f8b2b4e8c9512c7ba43c30438741440e9734
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-11-02 19:17:35 -0500

    simplify card passing component logic

--- src/scenes/Game/components/PassCards.js
+++ src/scenes/Game/components/PassCards.js
@@ -5,12 +5,10 @@ import { createSelector } from 'reselect';
 import styled from '@emotion/styled';
 
 import compact from 'lodash/compact';
-import every from 'lodash/every';
 import get from 'lodash/get';
 import isEmpty from 'lodash/isEmpty';
 import isEqual from 'lodash/isEqual';
 import last from 'lodash/last';
-import size from 'lodash/size';
 import some from 'lodash/some';
 
 import actions from 'actions';
@@ -48,45 +46,33 @@ const PassCards = () => {
     selectedCards,
   } = useSelector(mapStateToProps, isEqual);
 
-  const canPassWithCard = card => {
+  const canPassWithAnyCard = () => {
     if (legalPassesLimit < 1) return false;
 
-    return legalPassesCards.includes(card);
+    return some(compact(hand), card => legalPassesCards.includes(card));
   };
 
-  const canPassWithAnyCard = () => some(compact(hand), canPassWithCard);
+  const passWithSelectedCard = () => {
+    // TODO: technically we don't need this, since the socket server checks this.
+    // That said, having this check is an easy way to prevent sending too many
+    // unprocessable messages to the socket server.
+    if (isEmpty(selectedCards)) return;
 
-  const canDrop = item => canPassWithCard(item.card);
-
-  const drop = item => {
-    if (isEmpty(selectedCards)) {
-      io.send('passed', { card: item.card });
-    } else {
-      io.send('passed_with_many', { cards: selectedCards });
-    }
+    io.send('passed_with_many', { cards: selectedCards });
     dispatch(actions.game.selectedCards.clear());
+  };
+
+  const drop = () => {
+    passWithSelectedCard();
   };
 
   const [{ isOver }, dropRef] = useDrop({
     accept: 'CARD',
     drop,
-    canDrop,
     collect: monitor => ({
       isOver: !!monitor.isOver(),
     }),
   });
-
-  const passWithSelectedCard = () => {
-    try {
-      if (isEmpty(selectedCards)) return;
-      if (size(selectedCards) > legalPassesLimit) return;
-      if (!every(selectedCards, canPassWithCard)) return;
-
-      io.send('passed_with_many', { cards: selectedCards });
-    } finally {
-      dispatch(actions.game.selectedCards.clear());
-    }
-  };
 
   if (!canPassWithAnyCard()) return null;
 
