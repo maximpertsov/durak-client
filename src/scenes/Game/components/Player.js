f241ccf240cfbc94389f80de75608d9ea9874b80
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-11-22 19:17:57 -0500

    refactor

--- src/scenes/Game/components/Player.js
+++ src/scenes/Game/components/Player.js
@@ -186,6 +186,18 @@ const Player = ({ player, order }) => {
     );
   };
 
+  const renderCardsContext = () =>
+    attackLimit !== 'unlimited' && (
+      <UICard.Content extra>
+        <div>{`${cardCount} cards`}</div>
+        <WideScreenOnly>
+          <CardsWrapper>
+            <Cards cards={displayCards} scale={0.4} />
+          </CardsWrapper>
+        </WideScreenOnly>
+      </UICard.Content>
+    );
+
   const renderUICard = () => (
     <UICardWrapper faded={isOutOfPlay} fluid glowing={isUser}>
       <UICard.Content>
@@ -195,16 +207,7 @@ const Player = ({ player, order }) => {
         <Header size="small">{`${player}`}</Header>
         {getContext() && <UICard.Meta>{renderContext()}</UICard.Meta>}
       </UICard.Content>
-      {attackLimit !== 'unlimited' && (
-        <UICard.Content extra>
-          <div>{`${cardCount} cards`}</div>
-          <WideScreenOnly>
-            <CardsWrapper>
-              <Cards cards={displayCards} scale={0.4} />
-            </CardsWrapper>
-          </WideScreenOnly>
-        </UICard.Content>
-      )}
+      {renderCardsContext()}
     </UICardWrapper>
   );
 
